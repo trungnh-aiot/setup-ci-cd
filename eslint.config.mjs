a2319fcbf949196ee3cd9c1c4b36390e29d4990b
trungnh-aiot <trungnh@aiot-global.com>
2025-06-20 10:44:13 +0700

    feat:set up  prettier,eslint, husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,51 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  plugins: {
+    "simple-import-sort": simpleImportSort,
+    prettier: prettier,
+  },
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    plugins: ["prettier", "simple-import-sort"],
+    rules: {
+      semi: ["error"],
+      "prettier/prettier": "warn",
+
+      "@next/next/no-img-element": "warn",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-vars": "error",
+      "react/jsx-first-prop-new-line": ["error", "multiline"],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
+      "arrow-body-style": ["error", "as-needed"],
+      "prefer-template": "error",
+      "no-console": "warn",
+      camelcase: ["error", { ignoreDestructuring: true, properties: "never" }],
+      "no-underscore-dangle": "warn",
+
+      "import/newline-after-import": "warn",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+    settings: {
+      next: {
+        rootDir: ["src/app/"],
+      },
+    },
+  }),
 ];
 
 export default eslintConfig;
