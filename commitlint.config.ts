aac49d5247fdd74cd740c63e08724c31e2fd5c8e
trungnh-aiot <trungnh@aiot-global.com>
2025-06-20 11:09:39 +0700

    feat: setup husky message

---
+++ commitlint.config.ts
@@ -0,0 +1,20 @@
+import { type UserConfig } from "@commitlint/types";
+
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  prompt: {
+    settings: {
+      enableMultipleScopes: true,
+    },
+  },
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
+    ],
+    "subject-case": [2, "always", ["lower-case"]],
+  },
+} satisfies UserConfig;
+
+export default config;
